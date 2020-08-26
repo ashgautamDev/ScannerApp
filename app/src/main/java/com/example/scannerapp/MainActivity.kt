6c48077314db4c28e9bcbc5ea2051d816fc79f79
Ashish gautam <ashish30adev@gmail.com>
2020-08-25 23:46:50 -0400

    final app

---
+++ app/src/main/java/com/example/scannerapp/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.scannerapp
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val CAMERA_REQUEST_CODE = 101
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setUpPermission()
+        codescanner()
+    }
+
+    private fun codescanner() {
+        codeScanner = CodeScanner(this, code_scanner)
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    code_output.text = it.text
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                kotlin.run {
+                    Log.e("main", "somthing happens wrong : ${it.message}")
+                }
+            }
+
+            code_scanner.setOnClickListener {
+                codeScanner.startPreview()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        codeScanner.releaseResources()
+    }
+
+    private fun setUpPermission() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this,
+                        "You need to give permission for camera to scan ",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    //succefull no need to fuck here in this code
+                }
+            }
+        }
+
+    }
+
+}
